b04ec52cd2a0a2db8fade3c8c5b39d0ba6a371b3
MarkPWMason <63383779+MarkPWMason@users.noreply.github.com>
2020-04-09 06:05:29 +0100

    Create Commons.h

---
+++ Commons.h
@@ -0,0 +1,24 @@
+#pragma once
+
+struct Vector2D
+{
+	float x;
+	float y;
+
+	Vector2D()
+	{
+		x = 0.0f;
+		y = 0.0f;
+	}
+};
+
+enum SCREENS
+{
+	SCREEN_INTRO = 0,
+	SCREEN_MENU,
+	SCREEN_LEVEL1,
+	SCREEN_LEVEL2,
+	SCREEN_GAMEOVER,
+	SCREEN_HIGHSCORES
+};
+
